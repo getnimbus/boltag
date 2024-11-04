56dd4b76d767ec59ca16d2022b955e10c65430e3
binhnguyen2501 <binhq6@gmail.com>
2024-11-04 17:56:53 +0700

    feat/ref-dashboard-page

---
+++ src/components/Auth.tsx
@@ -0,0 +1,7 @@
+export const Auth = () => {
+  return (
+    <div className="rounded-[10px] py-2 px-3 text-white bg-[#1e96fc] text-sm font-medium cursor-pointer hover:bg-[#1878c9] transition-all">
+      Connect wallet
+    </div>
+  );
+};
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
 import Bolt from "../assets/bolt.png";
+import { Auth } from "./Auth";
 
 export const Header = () => {
   return (
     <div className="fixed top-0 left-0 z-30 w-full backdrop-blur-sm bg-white/30">
-      <div className="max-w-[1600px] m-auto xl:w-[88%] w-[90%] py-2">
+      <div className="max-w-[1600px] m-auto xl:w-[88%] w-[90%] py-2 flex justify-between items-center">
         <div className="flex items-center -ml-4">
           <img src={Bolt} alt="logo" className="w-12 h-12" />
           <div className="-ml-2 font-medium">Bolt</div>
         </div>
+        <Auth />
       </div>
     </div>
   );
